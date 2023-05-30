c633dad44ab81409bc2690b4a3bae100d80504c5
gpmatheus <gp.matheusluis@gmail.com>
2023-05-29 22:55:43 -0300

    fixed routes and remover unused routes

--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PageController;
@@ -41,8 +35,7 @@ use App\Http\Controllers\ProjectController;
     Route::delete('/projects/{id}', [ProjectController::class, 'destroy'])->name('projects.destroy');
     // end of the projects routes
 
-    //Route::get('/', function () {return redirect('/dashboard');})->middleware('auth');
-	Route::get('/', [HomeController::class, 'guest_home'])->name('home');
+	Route::get('/', [HomeController::class, 'guest_home'])->name('home')->middleware('guest');
 	Route::get('/register', [RegisterController::class, 'create'])->middleware('guest')->name('register');
 	Route::post('/register', [RegisterController::class, 'store'])->middleware('guest')->name('register.perform');
 	Route::get('/login', [LoginController::class, 'show'])->middleware('guest')->name('login');
@@ -52,6 +45,7 @@ use App\Http\Controllers\ProjectController;
 	Route::get('/change-password', [ChangePassword::class, 'show'])->middleware('guest')->name('change-password');
 	Route::post('/change-password', [ChangePassword::class, 'update'])->middleware('guest')->name('change.perform');
 	Route::get('/dashboard', [HomeController::class, 'index'])->name('dashboard')->middleware('auth');
+	
 Route::group(['middleware' => 'auth'], function () {
 	Route::get('/virtual-reality', [PageController::class, 'vr'])->name('virtual-reality');
 	Route::get('/rtl', [PageController::class, 'rtl'])->name('rtl');
